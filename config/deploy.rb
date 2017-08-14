8ec2de0c0b8c8a0fb3cdaf910682badd14bda902
Tran Chinh Thuc <tcthuc@cmc.com.vn>
2017-08-15 00:54:51 +0700

    ss

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,3 +10,10 @@ set :use_sudo, true
 set :bundle_binstubs, nil
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:legacy_restart'
+  end
+end
